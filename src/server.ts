deb6438f450d800424576c248458539952a37098
Shane Jennings <cshanejennings@gmail.com>
2025-09-02 13:29:38 -0400

    adding a resource to the server @31:00

--- src/server.ts
+++ src/server.ts
@@ -1,66 +1,84 @@
-import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
 import fs from "node:fs/promises";
 const server = new McpServer({
-    name: "test",
-    version: "1.0.0",
-    capabilities: {
-        resources: {},
-        tools: {},
-        prompts: {}
-    }
+  name: "test",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  },
 });
 
-server.tool("create-user", "Create a new user in the database", {
+server.resource(
+  "users",
+  "users:/all",
+  {
+    description: "Get all user data from the database",
+    title: "Users",
+    mimType: "application/json",
+  },
+  async (uri) => {
+    const users = await import("./data/users.json", {
+        with: { type: "json" },
+    }).then((m) => m.default);
+    return {
+      contents: [{ uri: uri.href, text: JSON.stringify(users), mimeType: "application/json" }],
+    };
+  }
+);
+
+server.tool(
+  "create-user",
+  "Create a new user in the database",
+  {
     name: z.string(),
     email: z.string().email(),
     address: z.string(),
-    phone: z.string()
-}, {
-   title: "Create User",
-   readOnlyHint: false,
-   destructiveHint: false,
-   idempotentHint: false,
-   openWorldHint: true,
-}, async (params) => {
+    phone: z.string(),
+  },
+  {
+    title: "Create User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async (params) => {
     try {
-        const id = await createUser(params);
-        return {
-            content: [
-                {type: "text", text: `User ${id} created successfully`}
-            ]
-        }
-
+      const id = await createUser(params);
+      return {
+        content: [{ type: "text", text: `User ${id} created successfully` }],
+      };
     } catch {
-        return {
-            content: [
-                {type: "text", text: "Failed to create user"}
-            ]
-        }
-
+      return {
+        content: [{ type: "text", text: "Failed to create user" }],
+      };
     }
-});
+  }
+);
 
 async function createUser(user: {
-    name: string,
-    email: string,
-    address: string,
-    phone: string,
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
 }) {
-    const users = await import("./data/users.json", {
-        with: { type: "json" }
-    }).then(m => m.default);
-    const id = users.length + 1;
-    users.push({ id, ...user });
-    await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2));
-    
-    return id;
+  const users = await import("./data/users.json", {
+    with: { type: "json" },
+  }).then((m) => m.default);
+  const id = users.length + 1;
+  users.push({ id, ...user });
+  await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2));
+
+  return id;
 }
 
 async function main() {
-    const transport = new StdioServerTransport();
-    await server.connect(transport);
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
 }
 
 main();
